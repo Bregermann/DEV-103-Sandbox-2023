e445e51d8d9bbaffac5554bed2e616528f80e896
Bregermann <30277759+Bregermann@users.noreply.github.com>
2023-02-21 15:49:12 -0500

    Added rolly ball and level 3 animations

---
+++ DMI Class Project the First/Assets/Scripts/Ball Levels/CameraFollow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    public float rotationSpeed = 10f;
+    public float maxVerticalRotation = 80f;
+
+    private float horizontalRotation = 0f;
+    private float verticalRotation = 0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, target.position.z + offset.z);
+
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        horizontalRotation += mouseX;
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -maxVerticalRotation, maxVerticalRotation);
+
+        transform.localRotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0f);
+    }
+}
